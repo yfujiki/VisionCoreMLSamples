37e234a1bae75b644603ffd812373edb9b9aba21
Yuichi Fujiki <fjk89025@gmail.com>
2019-06-06 10:56:52 +0800

    Getting feature values from CoreML model.

--- CameraCaptureRelatedSamples/DeeplabV3ViewController.swift
+++ CameraCaptureRelatedSamples/DeeplabV3ViewController.swift
@@ -8,10 +8,31 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class DeeplabV3ViewController: UIViewController {
 
     @IBOutlet weak var captureView: CameraCaptureView!
+
+    private lazy var visionModel: VNCoreMLModel? = {
+        let model = try? VNCoreMLModel(for: DeepLabV3().model)
+        return model
+    }()
+
+    private lazy var visionModelRequest: VNCoreMLRequest? = {
+        guard let visionModel = visionModel else {
+            return nil
+        }
+
+        let request = VNCoreMLRequest(model: visionModel) { (vnRequest, error) in
+            if let results = vnRequest.results as? [VNCoreMLFeatureValueObservation]{
+                print("Results \(results)")
+            }
+        }
+        request.imageCropAndScaleOption = .centerCrop
+
+        return request
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +48,20 @@ class DeeplabV3ViewController: UIViewController {
 }
 
 extension DeeplabV3ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ captureOutput: AVCaptureOutput, didOutput: CMSampleBuffer, from: AVCaptureConnection) {
+    func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from: AVCaptureConnection) {
+        guard let cvImage = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
 
+        let handler = VNImageRequestHandler(cvPixelBuffer: cvImage)
+
+        guard let request = visionModelRequest else {
+            return
+        }
+
+        DispatchQueue.global(qos: .userInteractive).async {
+            try? handler.perform([request])
+        }
     }
 }
 
